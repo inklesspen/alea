07de15bd549fb7588fc31b2ba4b3c62bf3c8ba1f
Rose Davidson <rose@inklesspen.com>
2017-10-24 23:15:21 -0700

    planning

--- alea.lisp
+++ alea.lisp
@@ -4,3 +4,13 @@
 
 ;;; "alea" goes here. Hacks and glory await!
 
+;;; interaction:
+;;; user types text
+;;; parsers (provided by context) transform text into nil or an AST
+;;; context transforms AST into a Displayable
+;;; alea renders Displayable to the appropriate channel
+
+
+;;; one campaign per discord server
+;;; use UUID-5 or UUID-4 for identifiers
+;;; provide a url where all public rolls made in a server can be viewed
